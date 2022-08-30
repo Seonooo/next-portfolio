5c729e3dec6fd71c4ea105f8d8d396401e0db315
Entityrn <eedd22@naver.com>
2022-08-30 20:10:31 +0900

    3day
    Notion api data

--- pages/projects.js
+++ pages/projects.js
@@ -1,40 +1,56 @@
 import Layout from "../components/layout"
 import Head from 'next/head'
 import {TOKEN, DATABASE_ID} from '../config/index'
+import ProjectItem from "../components/projects/project-image";
 
-export default function Projects(){
-    return (
-        <Layout>
-            <Head>
-                <title>Next 공부</title>
-                <meta name="description" content="Next 공부" />
-                <link rel="icon" href="/favicon.ico" />
-            </Head>
-            <h1>프로젝트</h1>
-        </Layout>
-    )
+export default function Projects({projects}){
+  return (
+    <Layout >
+      <div className="flex flex-col items-center justify-center min-h-screen mb-10 px-6">
+        <Head>
+            <title>Next 공부</title>
+            <meta name="description" content="Next 공부" />
+            <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <h1 className="text-3xl text-center font-bold sm:text-6xl">
+          총 프로젝트 :
+          <span className="pl-4 text-blue-500">{projects.results.length}</span>
+        </h1>
+        <div className="grid grid-clos-1 md:grid-cols-2 gap-5 py-10 m-6 w-full">
+          {projects.results.map((project)=> <ProjectItem key={project.id} data={project}/>)}
+        </div>
+      </div>
+    </Layout>
+  )
 }
 
 
-export async function getStaticProps(context) {
+export async function getStaticProps() {
 
-    const options = {
-        method: 'POST',
-        headers: {
-          Accept: 'application/json',
-          'Notion-Version': '2022-06-28',
-          'Content-Type': 'application/json',
-          Authorization: `Bearer ${TOKEN}`
-        },
-        body: JSON.stringify({page_size: 100})
-      };
-      
-        const res = await fetch(`https://api.notion.com/v1/databases/${DATABASE_ID}/query`, options);
-        const result = await res.json();
+  const options = {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      'Notion-Version': '2022-02-22',
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${TOKEN}`
+    },
+    body: JSON.stringify({
+      sorts : [
+        {
+          "property": "Name",
+          "direction":"ascending"
+        }
+      ],
+        page_size: 100
+    })
+  };
 
-        console.log(result);
+  const res = await fetch(`https://api.notion.com/v1/databases/${DATABASE_ID}/query`, options)
 
-    return {
-      props: {}, // will be passed to the page component as props
-    }
+  const projects = await res.json();
+
+  return {
+    props: {projects}, 
   }
+}
